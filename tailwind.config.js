162f2db9dba7f411b096d5cdeabbca966e140bef
Zendy <nguyenduc.051194@gmail.com>
2021-09-13 16:08:32 +0700

    config and responsive taiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '35': "35px",
+      },
+      colors: {
+        grey33: "#333"
+      },
+      // screens: {
+      //   'tablet': '640px',
+      //   // => @media (min-width: 640px) { ... }
+  
+      //   'laptop': '1024px',
+      //   // => @media (min-width: 1024px) { ... }
+  
+      //   'desktop': '1280px',
+      //   // => @media (min-width: 1280px) { ... }
+      // },
+    },
   },
   variants: {
     extend: {},
