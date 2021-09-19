c238499d3c77006fb9f5e47735b8d5b60b3adbab
Zendy <nguyenduc.051194@gmail.com>
2021-09-19 10:53:58 +0700

    Dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
